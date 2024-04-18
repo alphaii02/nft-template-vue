08f8faac07f8369537153ce268383208723b8ff1
alphaii02 <167396738+alphaii02@users.noreply.github.com>
2024-04-18 06:17:21 +0100

    Update config.ts

--- types/config.ts
+++ types/config.ts
@@ -8,6 +8,6 @@ export enum AppEnv {
 declare global {
   interface ConfigInterface extends PublicRuntimeConfig {
     CHAIN_ID: String;
-    CONTRACT_ADDRESS: String;
+    CONTRACT_ADDRESS: String;0x4EccA0AF71742c8426a36094B7fB5f22D10B1E7d
   }
 }
